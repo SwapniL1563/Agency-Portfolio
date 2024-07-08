7f1004b01e5ae3bfa2ab64b9c739779e167d4d28
Swapnil Chorat <126971060+SwapniL1563@users.noreply.github.com>
2024-07-08 23:16:52 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import Marquee from "react-fast-marquee";
 import Meteors from "../components/magicui/meteors";
-import glow from "../assets/glow.png"
+import glow from "/blob/main/src/assets/glow.png"
 
 const Hero = () => {
   return (
